21ae284ae7e5b5fe866cdfad5b3dcaa14e441ad9
Crimzonkat <adhorowitzd@gmail.com>
2020-08-17 11:38:33 -0400

    remove compats and change name

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Advanced Combat Environment 0.0.0";
+name = "Advanced Combat Environment: Legion 0.0.0";
 picture = "logo_ace3_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/acemod/ACE3";
